3e7cfd14ea2cfc04769defdf9ad8f0ad33607598
Giuseppe Tropea <giuseppetropea@Giuseppes-MacBook-Air.local>
2025-03-03 12:00:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ProfileImage />
+      <PersonalInfo />
+      <SocialLink />
     </div>
   );
 }
 
 export default App;
+
+//APP COMPONENT
+
+/* PROFILE IMAGE COMPONENT */
+
+function ProfileImage() {
+  return (
+    <div className="imgProfile">
+      <img src="/assets/images/avatar-jessica.jpeg" alt="jessica" />
+    </div>
+  );
+}
+
+/* PERSONAL INFORMATION */
+
+function PersonalInfo() {
+  return (
+    <>
+      <h2 style={{ fontWeight: "400", fontSize: "1.7rem" }}>Jessica Randall</h2>
+      <p style={{ color: "hsl(75, 94%, 57%)", fontWeight: "700" }}>
+        London, United Kingdom
+      </p>
+      <p style={{ margin: "1rem 0", fontWeight: "400" }}>
+        "Front-end Developer and avid reader"
+      </p>
+    </>
+  );
+}
+
+/* SOCIAL LINK */
+
+function SocialLink() {
+  return (
+    <div className="socialContainer">
+      <button>GitHub</button>
+      <button>Linkedin</button>
+      <button>Twitter</button>
+      <button>Instagram</button>
+      <button>YouTube</button>
+    </div>
+  );
+}
